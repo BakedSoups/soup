de3f04716cc0bdb8b074206fb7ce00acb53d392b
Alexander peczon <97857268+BakedSoups@users.noreply.github.com>
2024-10-20 22:06:01 +0000

    inputrc and autograder

--- home.nix
+++ home.nix
@@ -1,7 +1,28 @@
-{...}: {
+{pkgs, ...}: {
   home.file.".config/direnv/lib/uv.sh".source = ./scripts/uv.sh;
 
+  # autograder doesn't allow empty TOML key
+  xdg.configFile."grade/config.toml".source = (pkgs.formats.toml {}).generate "something" {
+    Canvas = {};
+    CanvasMapper = {};
+    Config = {};
+    Git = {};
+    Github = {};
+    Test = {
+      digital_path = "${pkgs.digital}/share/java/Digital.jar";
+    };
+  };
+
   programs = {
+    bash = {
+      enable = true;
+    };
+
+    readline = {
+      enable = true;
+      extraConfig = "set editing-mode vi";
+    };
+
     direnv = {
       enable = true;
       nix-direnv.enable = true;
@@ -20,13 +41,13 @@
       matchBlocks = {
         "stargate" = {
           hostname = "stargate.cs.usfca.edu";
-          user = "tltien";
+          user = "acpeczon";
           forwardAgent = true;
           identityFile = "/home/alex/.ssh/id_ed25519";
         };
         "beagle" = {
           hostname = "beagle";
-          user = "tltien";
+          user = "acpeczon";
           forwardAgent = true;
           identityFile = "/home/alex/.ssh/id_ed25519";
           proxyCommand = "ssh stargate -w %h:%p";
