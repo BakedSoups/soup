833fc4e347fec08cdf9444701c046842b23dbf83
BakedSoups <overbakedrice@gmail.com>
2024-10-20 22:57:30 +0000

    Remove GNOME

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,6 @@
   hardware.graphics.enable = true;
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
   services.desktopManager.plasma6.enable = true;
   systemd.services.display-manager.restartIfChanged = false;
 
